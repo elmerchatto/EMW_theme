27f2368dcfe36493218be97c54b0253472fa93a2
Elmer <iamelmerchatto@gmail.com>
2022-08-31 02:01:44 +0800

    Added Sticky Header

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 jQuery(document).ready(function(){
 
+    $(window).scroll(function () {
+        if ($(document).scrollTop() > 80) {
+          $('header').addClass('sticky');
+        } else {
+          $('header').removeClass('sticky');
+        }
+      });
+      
 });
