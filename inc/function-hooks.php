86bc67b7463ff07183f2ca5955f36713ab02c427
Elmer <iamelmerchatto@gmail.com>
2022-08-30 22:04:42 +0800

    Update Enqueue

--- inc/function-hooks.php
+++ inc/function-hooks.php
@@ -36,18 +36,23 @@ function emw_register_theme_locations() {
  * Enqueue Scripts
  */
 
+
+/** 
+ * Enqueue Scripts
+ */
+
 add_action( 'wp_enqueue_scripts', 'enqueue_scripts__' );
 function enqueue_scripts__() {
 
-  if(@get_field( 'animsition' )) :
+  if(@get_field( 'animsition','option' )) :
 
     wp_enqueue_style( 'animsition-css', 'https://cdnjs.cloudflare.com/ajax/libs/animsition/3.2.1/css/animsition.min.css' );
-    wp_enqueue_script( 'animsition-js', 'https://cdnjs.cloudflare.com/ajax/libs/animsition/3.2.1/js/animsition.min.js' );
+    wp_enqueue_script( 'animsition-js', 'https://cdnjs.cloudflare.com/ajax/libs/animsition/3.2.1/js/animsition.min.js', array('jquery'), '1.0', true  );
 
   endif;  
   
 
-  if(@get_field( 'barba' )) :
+  if(@get_field( 'barba','option' )) :
 
     wp_enqueue_script( 'barba-css', 'https://unpkg.com/@barba/css' );
     // wp_enqueue_script( 'barba-js', 'https://cdnjs.cloudflare.com/ajax/libs/barba.js/0.0.2/barba.min.js' ); 
@@ -55,82 +60,82 @@ function enqueue_scripts__() {
 
   endif;  
 
-  if(@get_field( 'images_loaded' )) :
+  if(@get_field( 'images_loaded','option' )) :
 
-    wp_enqueue_script( 'images-loaded-js', 'https://cdnjs.cloudflare.com/ajax/libs/jquery.imagesloaded/5.0.0/imagesloaded.min.js' );
+    wp_enqueue_script( 'images-loaded-js', 'https://cdnjs.cloudflare.com/ajax/libs/jquery.imagesloaded/5.0.0/imagesloaded.min.js', array('jquery'), '1.0', true  );
 
   endif; 
 
-  if(@get_field( 'fullpage_js' )) :
+  if(@get_field( 'fullpage_js','option' )) :
 
     wp_enqueue_style( 'fullpage-css', 'https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.7/jquery.fullpage.min.css' );
-    wp_enqueue_script( 'fullpage-js', 'https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.7/jquery.fullpage.min.js' );
-    wp_enqueue_script( 'fullpage-extensions-js', 'https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.7/jquery.fullpage.extensions.min.js' );
-    wp_enqueue_script( 'fullpage-scroll-overflow-js', 'https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.7/vendors/scrolloverflow.min.js' );
-    wp_enqueue_script( 'fullpage-easings-js', 'https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.7/vendors/jquery.easings.min.js.js' );
+    wp_enqueue_script( 'fullpage-js', 'https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.7/jquery.fullpage.min.js', array('jquery'), '1.0', true  );
+    wp_enqueue_script( 'fullpage-extensions-js', 'https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.7/jquery.fullpage.extensions.min.js', array('jquery'), '1.0', true  );
+    wp_enqueue_script( 'fullpage-scroll-overflow-js', 'https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.7/vendors/scrolloverflow.min.js', array('jquery'), '1.0', true  );
+    wp_enqueue_script( 'fullpage-easings-js', 'https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.7/vendors/jquery.easings.min.js.js', array('jquery'), '1.0', true  );
 
   endif; 
   
 
-  if(@get_field( 'gsap' )) :
+  if(@get_field( 'gsap','option' )) :
 
     wp_enqueue_script( 'gsap-css', '' );
     wp_enqueue_script( 'gsap-js', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.0/gsap.min.js' );
-    wp_enqueue_script( 'gsap-easepack-js', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.0/EasePack.min.js' );
-    wp_enqueue_script( 'gsap-scrolltrigger-js', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.0/ScrollTrigger.min.js' );
+    wp_enqueue_script( 'gsap-easepack-js', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.0/EasePack.min.js', array('jquery'), '1.0', true  );
+    wp_enqueue_script( 'gsap-scrolltrigger-js', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.0/ScrollTrigger.min.js', array('jquery'), '1.0', true  );
 
   endif;
 
-  if(@get_field( 'scroll_magic' )) :
+  if(@get_field( 'scroll_magic','option' )) :
 
-    wp_enqueue_script( 'scroll-magic-js', 'https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.8/ScrollMagic.min.js' );
-    wp_enqueue_script( 'scroll-magic-timelinelite-js', 'http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TimelineLite.min.js' );
-    wp_enqueue_script( 'scroll-magic-tweenmax-js',  get_template_directory_uri(). '/js/ext/tweenmax.js' );
-    wp_enqueue_script( 'scroll-magic-gsap-animation-js', 'https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.8/plugins/animation.gsap.min.js' );
-    wp_enqueue_script( 'scroll-magic-indicators-js', 'https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.8/plugins/debug.addIndicators.min.js' );
+    wp_enqueue_script( 'scroll-magic-js', 'https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.8/ScrollMagic.min.js', array('jquery'), '1.0', true  );
+    wp_enqueue_script( 'scroll-magic-timelinelite-js', 'http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TimelineLite.min.js', array('jquery'), '1.0', true  );
+    wp_enqueue_script( 'scroll-magic-tweenmax-js',  get_template_directory_uri(). '/js/ext/tweenmax.js', array('jquery'), '1.0', true  );
+    wp_enqueue_script( 'scroll-magic-gsap-animation-js', 'https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.8/plugins/animation.gsap.min.js', array('jquery'), '1.0', true  );
+    wp_enqueue_script( 'scroll-magic-indicators-js', 'https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.8/plugins/debug.addIndicators.min.js', array('jquery'), '1.0', true  );
 
   endif; 
 
-  if(@get_field( 'slick' )) :
+  if(@get_field( 'slick','option' )) :
 
     wp_enqueue_style( 'slick-css', 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css' );
-    wp_enqueue_style( 'slick-theme', get_template_directory_uri() . '/css/ext/slick-theme.css' );
-    wp_enqueue_script( 'slick-js', 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js' );
+    wp_enqueue_style( 'slick-theme', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css' );
+    wp_enqueue_script( 'slick-js', 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js', array('jquery'), '1.0', true );
 
   endif;  
 
-  if(@get_field( 'bootstrap_js' )) :
+  if(@get_field( 'bootstrap_js','option' )) :
 
-    wp_enqueue_script( 'bootstrap-js', 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.0/js/bootstrap.min.js' );
+    wp_enqueue_script( 'bootstrap-js', 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.0/js/bootstrap.min.js', array('jquery'), '1.0', true  );
 
   endif; 
   
-  if(@get_field( 'flickity' )) :
+  if(@get_field( 'flickity','option' )) :
 
     wp_enqueue_style( 'flickity-css', 'https://cdnjs.cloudflare.com/ajax/libs/animsition/3.2.1/css/animsition.min.css' );
-    wp_enqueue_script( 'flickity-js', 'https://cdnjs.cloudflare.com/ajax/libs/flickity/3.0.0/flickity.pkgd.min.js' );
+    wp_enqueue_script( 'flickity-js', 'https://cdnjs.cloudflare.com/ajax/libs/flickity/3.0.0/flickity.pkgd.min.js', array('jquery'), '1.0', true  );
 
   endif; 
 
-  if(@get_field( 'infinite_scroll' )) :
+  if(@get_field( 'infinite_scroll','option' )) :
 
-    wp_enqueue_script( 'infinite-scroll-js', 'https://cdnjs.cloudflare.com/ajax/libs/jquery-infinitescroll/4.0.1/infinite-scroll.pkgd.min.js' );
-
-  endif; 
-
-  if(@get_field( 'isotope' )) :
-
-    wp_enqueue_script( 'isotope-js', 'https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.min.js' );
+    wp_enqueue_script( 'infinite-scroll-js', 'https://cdnjs.cloudflare.com/ajax/libs/jquery-infinitescroll/4.0.1/infinite-scroll.pkgd.min.js', array('jquery'), '1.0', true  );
 
   endif; 
 
-  if(@get_field( 'masonry' )) :
+  if(@get_field( 'isotope','option' )) :
 
-    wp_enqueue_script( 'masonry-js', 'https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js' );
+    wp_enqueue_script( 'isotope-js', 'https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.min.js', array('jquery'), '1.0', true  );
 
   endif; 
 
-  if(@get_field( 'lottie' )) :
+  if(@get_field( 'masonry','option' )) :
+
+    wp_enqueue_script( 'masonry-js', 'https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js', array('jquery'), '1.0', true  );
+
+  endif; 
+
+  if(@get_field( 'lottie','option' )) :
 
     wp_enqueue_script( 'lottie-js', 'https://cdnjs.cloudflare.com/ajax/libs/lottie-web/5.9.6/lottie.min.js' );
     wp_enqueue_script( 'lottie-canvas-js', 'https://cdnjs.cloudflare.com/ajax/libs/lottie-web/5.9.6/lottie_canvas.min.js' );
@@ -142,10 +147,10 @@ function enqueue_scripts__() {
   endif; 
 
 
-  if(@get_field( 'magnific_popup' )) :
+  if(@get_field( 'magnific_popup','option' )) :
 
     wp_enqueue_style( 'magnefic-css', 'https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.css' );
-    wp_enqueue_script( 'magnefic-js',  'https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js' );
+    wp_enqueue_script( 'magnefic-js',  'https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js', array('jquery'), '1.0', true  );
 
   endif;  
 
